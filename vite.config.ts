f63c2d2e39f5d711bb666916c46aab80685151b4
Muniox <muniox20@gmail.com>
2022-12-12 14:57:24 +0100

    new vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
+import autoprefixer from 'autoprefixer'
+import postcssNested from 'postcss-nested'
+import postcssScss from 'postcss-scss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://muniox.github.io/nft-preview-card-component/',
   css: {
-    devSourcemap: true,
+    postcss: {
+      syntax: postcssScss,
+      plugins: [
+        autoprefixer({}),
+        postcssNested({})
+      ]
+    }
   },
   plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
+    react()
   ]
 })
